970e891b03eadc256acd93529a44be3e9711ccae
djangoconfire <djangopycon@gmail.com>
2017-02-25 22:44:01 +0530

    updated production.py

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -14,5 +14,5 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ritu-blog.herokuapp.com']
 
